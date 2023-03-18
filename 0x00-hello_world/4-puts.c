4f18556d1da82f4231bd1e5fe4a08f121451fc2e
METERO Aloys <aloysmet@gmail.com>
2023-03-18 04:57:56 -0700

    This how to print String using Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - is the starting point of program
+ * Description: This program is used to print message using puts
+ * Return: it return 0 always -success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
