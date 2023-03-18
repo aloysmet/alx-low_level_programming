ecd80db00730b1ccbf2f21416bac6d887e7f4133
METERO Aloys <aloysmet@gmail.com>
2023-03-18 05:06:04 -0700

    This commit ahows how to print string using printf function in c programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this starting point pf program
+ * Description: this program is using printf to display message
+ * Return: always return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
