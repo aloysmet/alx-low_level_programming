d1b6d2765e930ffaa10c5305d0cec1db2377b079
METERO Aloys <aloysmet@gmail.com>
2023-03-19 02:24:55 -0700

    This program is used to print alphabet but except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this is startip point of program
+ * Description: this program print lower alphabet except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c += 1)
+	{
+		if( c == 'q' || c == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
